62e770db7529ec008c56eb1108e6f46aa597ec42
WieeRd <verywieerd@gmail.com>
2023-10-23 07:00:26 +0900

    feat(shuffle): add const array generator `enumerate()`

--- shufflesort/src/main.rs
+++ shufflesort/src/main.rs
@@ -1,3 +1,32 @@
+/// Creates an array of `u8` where the value of each element is its index number.
+///
+/// e.g. `enumerate::<N>() == [1u8, 2u8, ..., N-1]`
+const fn enumerate<const N: usize>() -> [u8; N] {
+    // # FAQ (the voices inside my head)
+    //
+    // 1. Isn't this supported by the language?
+    //
+    // Well yes, but actually no. We have:
+    //
+    // - `core::array::from_fn(|i| i)` ...except it's not `const`
+    // - `(0..N).collect()` ...except it only works on `Vec`
+    //
+    // Until const trait and fixed sized iterator stuffs are stabilized, this is all we've got.
+    //
+    // 2. `T` instead of `u8`?
+    //
+    // We need to convert `usize` index to `T` value in that case.
+    // Which requires `From`, which is a trait, which, cannot be `const`.
+    let mut arr = [0u8; N];
+    let mut i = 0;
+    while i < arr.len() {
+        arr[i] = i as u8;
+        i += 1;
+    }
+    arr
+}
+
 fn main() {
-    println!("Hello, world!");
+    const A: [u8; 3] = enumerate();
+    println!("{A:?}");
 }
