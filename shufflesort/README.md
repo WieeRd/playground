cdef02c530a16e1a6a624f13c45424a36499a12d
WieeRd <verywieerd@gmail.com>
2023-10-23 18:52:21 +0900

    feat(shuffle): add `shuffle()` for generating sort order

--- shufflesort/README.md
+++ shufflesort/README.md
@@ -1,18 +1,23 @@
-# Shufflesort
+# Shuffle Sort
 
 Wacky woohoo sorting problem where buffering matters more than sorting.
 
 ## Input
 
-- File name
-  - Contains 1M words
-    - Separated by newline
-    - 5~20 characters
-    - Lowercase (`[a-z]`)
-- Shuffled sorting order
-  - One-to-one mapping of `[a-z] -> [a-z]`
+- A file containing 1M words
+  - Separated by newline
+  - 5~20 characters
+  - Lowercase `[a-z]`
+
+- Shuffled sorting order of alphabets
+  1. Swap each element at index `2K` with the element at index `2K + 1`.\
+    e.g. `[1, 2, 3, 4]` → `[2, 1, 4, 3]`
+  2. Swap the first half with the last half of the array.\
+    e.g. `[1, 2, 3, 4]` → `[3, 4, 1, 2]`
+
+In short, sort by `"mporqtsvuxwzybadcfehgjilkn"` instead of `"abcdefghijklmnopqrstuvwxyz"`.
 
 ## Output
 
 - Sort the words based on the given order
-- Print to stdout, each word separated by a newline
+- Print the words to stdout, with each word separated by newline
--- shufflesort/src/main.rs
+++ shufflesort/src/main.rs
@@ -1,23 +1,27 @@
-/// Creates an array of `u8` where the value of each element is its index number.
+#![doc = include_str!("../README.md")]
+
+const ALPHABETS: usize = 26;
+
+/// Creates an array of `u8` where each element is set to its index (`a[i] == i`).
 ///
 /// e.g. `enumerate::<N>() == [1u8, 2u8, ..., N-1]`
 const fn enumerate<const N: usize>() -> [u8; N] {
-    // # FAQ (the voices inside my head)
-    //
-    // 1. Isn't this supported by the language?
-    //
-    // Well yes, but actually no. We have:
-    //
-    // - `core::array::from_fn(|i| i)` ...except it's not `const`
-    // - `(0..N).collect()` ...except it only works on `Vec`
-    //
-    // Until const trait and fixed sized iterator stuffs are stabilized, this is all we've got.
-    //
-    // 2. `T` instead of `u8`?
-    //
-    // We need to convert `usize` index to `T` value in that case.
-    // Which requires `From`, which is a trait, which, cannot be `const`.
-    let mut arr = [0u8; N];
+    //! # FAQ (the voices inside my head)
+    //!
+    //! 1. Isn't this supported by the language?
+    //!
+    //! Well yes, but actually no. We have:
+    //!
+    //! - `core::array::from_fn(|i| i)` ...except it's not `const`.
+    //! - `(0..N).collect()` ...except it only works for `Vec`.
+    //!
+    //! Until const trait and fixed sized iterator stuffs are stabilized, this is all we've got.
+    //!
+    //! 2. `T` instead of `u8`?
+    //!
+    //! We need to convert `usize` index to `T` value in that case.
+    //! Which requires `From`, which is a trait, which, cannot be `const`.
+    let mut arr = [0; N];
     let mut i = 0;
     while i < arr.len() {
         arr[i] = i as u8;
@@ -26,7 +30,31 @@ const fn enumerate<const N: usize>() -> [u8; N] {
     arr
 }
 
+/// Shuffles the given array with the steps described in [`README.md`](index.html).
+///
+/// # Panics
+///
+/// Given array must have even length (`N % 2 == 0`).
+const fn shuffle<const N: usize>(mut arr: [u8; N]) -> [u8; N] {
+    assert!(N % 2 == 0, "given array must have even length");
+
+    let mut i = 0;
+    while i < N {
+        (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+        i += 2;
+    }
+
+    let mut i = 0;
+    while i < N / 2 {
+        (arr[i], arr[N / 2 + i]) = (arr[N / 2 + i], arr[i]);
+        i += 1;
+    }
+
+    arr
+}
+
 fn main() {
-    const A: [u8; 3] = enumerate();
-    println!("{A:?}");
+    const A: [u8; ALPHABETS] = shuffle(enumerate());
+
+    println!("{:?}", std::str::from_utf8(&A.map(|i| i + 'a' as u8)).unwrap());
 }
