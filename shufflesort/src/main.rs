df1514ef04487a9bd2e38c8e4e2651064703de8f
WieeRd <verywieerd@gmail.com>
2023-10-30 10:21:07 +0900

    chore(shuffle): just messing around with input buf

--- shufflesort/src/main.rs
+++ shufflesort/src/main.rs
@@ -1,13 +1,20 @@
 #![doc = include_str!("../README.md")]
 
+use std::{env, fs, io};
+
+#[allow(dead_code)]
 mod shuffled;
 
-fn main() {
-    let mut alphabets = String::from("abcdefghijklmnopqrstuvwxyz");
-    unsafe {
-        alphabets
-            .as_bytes_mut()
-            .sort_unstable_by_key(|c| shuffled::sort_key(c));
-    }
-    println!("{}", alphabets);
+// const WORD_COUNT: usize = 1_000_000;
+// const WORD_LEN_MIN: usize = 5;
+// const WORD_LEN_MAX: usize = 20;
+
+fn main() -> Result<(), io::Error> {
+    let input = match env::args_os().nth(1) {
+        Some(path) => fs::read(path)?,
+        None => fs::read("input.txt")?,
+    };
+
+    dbg!(input.len());
+    Ok(())
 }
